90954add61d9b5fe86d6d21b7abb7597eaf0c7aa
Lunacy88 <d.zayiceva@gmail.com>
2019-05-01 12:01:17 +0300

    Добавлена поддержка PostgreSQL

--- app/config/config.example.php
+++ app/config/config.example.php
@@ -4,8 +4,15 @@ return new \Phalcon\Config([
     'module_default' => 'www',
     'module_api' => 'api',
     'langs' => ['ru', 'en', 'fr', 'de'],
+//    'db_adapter' => 'Postgresql',
+//    'database' => [
+//        'host'        => 'localhost',
+//        'username'    => 'postgres',
+//        'password'    => '',
+//        'dbname'      => 'blank',
+//    ],
+    'db_adapter' => 'Mysql',
     'database' => [
-        'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
         'password'    => '',
